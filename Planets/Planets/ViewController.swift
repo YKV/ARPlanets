d9547850f72f853b462f057eff5c24e48b0a3fff
Eugene <eugene@smooth.tech>
2019-02-18 12:50:21 -0500

    added moon with Rotation

--- Planets/Planets/ViewController.swift
+++ Planets/Planets/ViewController.swift
@@ -30,17 +30,6 @@ class ViewController: UIViewController {
         addVenus()
         addEarth()
     }
-    
-    private func planet(geometry: SCNGeometry, diffuse: UIImage, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3) -> SCNNode{
-        let planetNode = SCNNode()
-        planetNode.geometry = geometry
-        planetNode.geometry?.firstMaterial?.diffuse.contents = diffuse
-        planetNode.geometry?.firstMaterial?.specular.contents = specular
-        planetNode.geometry?.firstMaterial?.emission.contents = emission
-        planetNode.geometry?.firstMaterial?.normal.contents = normal
-        planetNode.position = position
-        return planetNode
-    }
 }
 
 
@@ -51,14 +40,15 @@ extension ViewController {
     private func addSun() {
         sun.geometry?.firstMaterial?.diffuse.contents = UIImage.init(named: "sun")
         sun.position = SCNVector3.init(0, 0, -1)
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degressToRadians), z: 0, duration: 8)
-        let forever = SCNAction.repeatForever(action)
-        sun.runAction(forever)
+        let sunAction = Rotation(time: 8)
+        sun.runAction(sunAction)
         self.sceneView.scene.rootNode.addChildNode(sun)
     }
     
     private func addEarth() {
         let earthParent = SCNNode()
+        let moonParent = SCNNode()
+        moonParent.position = SCNVector3.init(1.2, 0, 0)
         earthParent.position = SCNVector3.init(0, 0, -1)
         let earth = planet(geometry: SCNSphere.init(radius: 0.2),
                            diffuse: UIImage.init(named: "Earth day") ?? UIImage(),
@@ -66,11 +56,20 @@ extension ViewController {
                            emission: UIImage.init(named: "Earth emission"),
                            normal: UIImage.init(named: "Earth normal"),
                            position: SCNVector3.init(1.2, 0, 0))
-        earthParent.addChildNode(earth)
+        let moon = addMoon()
+        let moonAction = Rotation(time: 5)
+        moonParent.runAction(moonAction)
+        earth.addChildNode(moon)
         
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degressToRadians), z: 0, duration: 14)
-        let forever = SCNAction.repeatForever(action)
-        earthParent.runAction(forever)
+        let earthAction = Rotation(time: 8)
+        earth.runAction(earthAction)
+        
+        moonParent.addChildNode(moon)
+        earthParent.addChildNode(earth)
+        earthParent.addChildNode(moonParent)
+        
+        let earthParentAction = Rotation(time: 14)
+        earthParent.runAction(earthParentAction)
         self.sceneView.scene.rootNode.addChildNode(earthParent)
     }
     
@@ -83,11 +82,48 @@ extension ViewController {
                            emission: UIImage.init(named: "venus_atmosphere"),
                            normal: nil,
                            position: SCNVector3.init(0.7, 0, 0))
+        let venusAction = Rotation(time: 8)
+        venus.runAction(venusAction)
         venusParent.addChildNode(venus)
         
-        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degressToRadians), z: 0, duration: 10)
-        let forever = SCNAction.repeatForever(action)
-        venusParent.runAction(forever)
+        let venusParentAction = Rotation(time: 10)
+        venusParent.runAction(venusParentAction)
         self.sceneView.scene.rootNode.addChildNode(venusParent)
     }
+    
+    
+    private func addMoon() -> SCNNode{
+        let moon = planet(geometry: SCNSphere.init(radius: 0.05),
+                           diffuse: UIImage.init(named: "moon") ?? UIImage(),
+                           specular: nil,
+                           emission: nil,
+                           normal: nil,
+                           position: SCNVector3.init(0, 0, -0.3))
+        return moon
+    }
+    
+}
+
+
+// MARK: - Generic private methods
+
+extension ViewController {
+    
+    private func planet(geometry: SCNGeometry, diffuse: UIImage, specular: UIImage?, emission: UIImage?, normal: UIImage?, position: SCNVector3) -> SCNNode{
+        let planetNode = SCNNode()
+        planetNode.geometry = geometry
+        planetNode.geometry?.firstMaterial?.diffuse.contents = diffuse
+        planetNode.geometry?.firstMaterial?.specular.contents = specular
+        planetNode.geometry?.firstMaterial?.emission.contents = emission
+        planetNode.geometry?.firstMaterial?.normal.contents = normal
+        planetNode.position = position
+        return planetNode
+    }
+    
+    private func Rotation(time: TimeInterval) -> SCNAction {
+        let rotation = SCNAction.rotateBy(x: 0, y: CGFloat(360.degressToRadians), z: 0, duration: time)
+        let foreverRotation = SCNAction.repeatForever(rotation)
+        return foreverRotation
+    }
+    
 }
